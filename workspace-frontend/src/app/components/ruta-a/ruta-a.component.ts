d9418e6d3aa3042898b067eb48f6554c03e2e0e1
Nicolás Bustamante <nicolas.bustamante@haulmer.com>
2023-08-09 13:24:44 -0400

    Fix ruta a

--- workspace-frontend/src/app/components/ruta-a/ruta-a.component.ts
+++ workspace-frontend/src/app/components/ruta-a/ruta-a.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ruta-a',
   template: `
-    <p>
-      ruta-a works!
-    </p>
+    <div class="content" role="main">
+      <a href="/">Ir a inicio</a>
+      <h2>Ruta A</h2>
+      <p>Sin el iframe</p>
+    </div>
   `,
   styleUrls: ['./ruta-a.component.scss']
 })
